47d61e782a3837551f77d987097839a238c5df6b
sh4aadman <sh4adman@gmail.com>
2026-07-21 12:57:06 +0600

    header created

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,12 +1,13 @@
-import moment from "moment";
+import { format } from "date-fns";
 import logo from "../../../assets/icons/logo.png"
+import { date } from "../../../utils/constants";
 
 function Header() {
   return (
     <header className='text-center pt-12'>
-            <img className='mx-auto' src={logo} alt="logo-picture" />
+            <img className='mx-auto' src={logo} alt="logo" />
             <p className='mt-5'>Journalism Without Fear or Favour</p>
-            <h2 className="text-2xl mt-2">{moment().format('dddd, MMMM D, YYYY')}</h2>
+            <h2 className="text-2xl mt-2.5">{format(date, 'EEEE, MMMM d, yyyy')}</h2>
         </header>
   )
 }
