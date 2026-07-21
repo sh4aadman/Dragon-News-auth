8327f96547902862a08a3c2c8f7b2fba107c51ce
sh4aadman <sh4adman@gmail.com>
2026-07-21 17:37:40 +0600

    updated

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,15 +1,18 @@
 import { format } from "date-fns";
-import logo from "../../../assets/icons/logo.png"
+import logo from "../../../assets/icons/logo.png";
 import { date } from "../../../utils/constants";
 
 function Header() {
   return (
-    <header className='text-center pt-12'>
-            <img className='mx-auto' src={logo} alt="logo" />
-            <p className='mt-5'>Journalism Without Fear or Favour</p>
-            <h2 className="text-2xl mt-2.5">{format(date, 'EEEE, MMMM d, yyyy')}</h2>
-        </header>
-  )
+    <header className="text-center pt-12">
+      <img className="mx-auto" src={logo} alt="logo" />
+      <p className="mt-5 text-base-300 leading-7">Journalism Without Fear or Favour</p>
+      <p className="text-2xl mt-2.5 leading-7">
+        <span className="text-primary">{format(date, "EEEE")}</span>{", "}
+        <span className="text-base-300">{format(date, "MMMM d, yyyy")}</span>
+      </p>
+    </header>
+  );
 }
 
 export default Header;
